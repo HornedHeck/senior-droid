57ef616201f46cb443106ce5eb1d9882eabb6926
Horned Heck <holitrixpecora@gmail.com>
2022-08-19 21:26:49 +0200

    buildSrc: buildSrc versions&dependencies without gradle version catalogs

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,3 +1,17 @@
 object MainLibs {
 
+    const val KOTLIN = "1.7.10"
+    const val JUNIT4 = "4.13.2"
+
+}
+
+object Android{
+
+    const val ANDROID_PLUGINS = "7.2.2"
+    const val KTX_CORE = "1.8.0"
+    const val COMPAT = "1.5.0"
+    const val GOOGLE_MATERIAL = "1.6.1"
+    const val JUNIT_ANDROID = "1.1.3"
+    const val ESPRESSO = "3.4.0"
+
 }
